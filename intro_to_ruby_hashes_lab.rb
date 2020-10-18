82d1aefae8d83f0a0dab3d32e18c8d29a13b8c8a
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-18 03:04:25 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,6 +1,6 @@
 def base_hash
-  railroads = {}
-  railroads
+  monopoly = {:railroads}
+  monopoly
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
 end
