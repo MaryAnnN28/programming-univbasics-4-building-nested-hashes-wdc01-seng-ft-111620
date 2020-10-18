a56147a4e4db23b9780b971e5021535cb15539a4
Mary Ann Navarrete <maryann.navarrete@gmail.com>
2020-10-18 02:57:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -4,6 +4,7 @@ def base_hash
   # keyword "return." Nice and neat.
 end
 railroads
+end
 
 def monopoly_with_second_tier
   # When you start writing the implementation for this method, copy the Hash
